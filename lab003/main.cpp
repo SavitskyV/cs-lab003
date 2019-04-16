8399ef4b95b2c9f42a4d7b1ef7a584be003e7a8d
Valentin Savitsky <sevendays559@gmail.com>
2019-04-16 14:26:26 +0300

    code: оформление гистограммы

--- lab003/main.cpp
+++ lab003/main.cpp
@@ -38,38 +38,47 @@ vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count) {
     return bins;
 }
 
-void show_histogram_text(const vector<size_t>& bins) {
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-    
-    size_t max_count = 0;
-    for (size_t count : bins) {
-        if (count > max_count) {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed = max_count > MAX_ASTERISK;
-    
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg width='" << width << "' height='" << height << "' "
+    << "viewBox='0 0 " << width << " " << height << "' "
+    << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void
+svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" <<text<< "</text>";
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "#EF867B"){
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    //svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    //svg_rect(TEXT_WIDTH, 0, bins[0] * 10, BIN_HEIGHT);
+    double top = 0;
     for (size_t bin : bins) {
-        if (bin < 100) {
-            cout << ' ';
-        }
-        if (bin < 10) {
-            cout << ' ';
-        }
-        cout << bin << "|";
-        
-        size_t height = bin;
-        if (scaling_needed) {
-            const double scaling_factor = (double)MAX_ASTERISK / max_count;
-            height = (size_t)(bin * scaling_factor);
-        }
-        
-        for (size_t i = 0; i < height; i++) {
-            cout << '*';
-        }
-        cout << '\n';
+        const double bin_width = 10 * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        top += BIN_HEIGHT;
     }
+    svg_end();
 }
 
 
@@ -89,7 +98,7 @@ int main() {
     // Обработка данных
     const vector<size_t>  bins = make_histogram(numbers, bin_count);
     
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
     
     return 0;
 }
