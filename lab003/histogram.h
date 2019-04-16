a43aa6f94ad1ebcf7574ad70ab19219e1628b605
Valentin Savitsky <sevendays559@gmail.com>
2019-04-16 16:25:56 +0300

    code: заголовочный файл

--- lab003/histogram.h
+++ lab003/histogram.h
@@ -2,9 +2,17 @@
 #define histogram_h
 
 #include <vector>
+#include <string>
 
 using namespace std;
 
 void find_minmax(const vector<double>& numbers, double& min, double& max);
+
+void svg_begin(double width, double height);
+void svg_end();
+void svg_text(double left, double baseline, string text);
+void svg_rect(double x, double y, double width, double height, string stroke, string fill);
+void show_histogram_svg(const vector<size_t>& bins);
+
 
 #endif /* histogram_h */
